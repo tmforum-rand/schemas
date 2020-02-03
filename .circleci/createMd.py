5eccc04ae06f68e58b95a0f6a1b75ccdbc2af288
Henrique Rodrigues <hrodrigues@tmforum.org>
2020-02-03 16:51:57 +0000

    details dropdown

--- .circleci/createMd.py
+++ .circleci/createMd.py
@@ -9,21 +9,21 @@ This repository contains the collection of JSON-Schema files that define the ent
 | Level-0 |  |
 |-----------------|---|
 | [Marketing/Sales](https://github.com/tmforum-rand/schemas/tree/master/MarketingSales) | The Market/Sales domain supports the sales and marketing activities needed to gain business from customers and potential customers. |
-| | <schemas>${MarketingSales}<br></schemas> | |
+| | <details>${MarketingSales}<br></details> | |
 | [Product](https://github.com/tmforum-rand/schemas/tree/master/Product/) | The Product domain is concerned with the lifecycle of products offered to and procured by customers. |
-| | <schemas>${Product}<br></schemas> | |
+| | <details>${Product}<br></details> | |
 | [Customer](https://github.com/tmforum-rand/schemas/tree/master/Customer) | The Customer domain represents individuals or organizations that obtain products from an enterprise, such as a service provider. It represents of all types of contact with the customer, the management of the relationship, and the administration of customer data. |
-| | <schemas>${Customer}<br></schemas> | |
+| | <details>${Customer}<br></details> | |
 | [Service](https://github.com/tmforum-rand/schemas/tree/master/Service) | The Service Domain is concerned with the definition, development, and operational aspects of Services used to realize offerings to the market. |
-| | <schemas>${Service}<br></schemas> | |
+| | <details>${Service}<br></details> | |
 | [Resource](https://github.com/tmforum-rand/schemas/tree/master/Resource) | The Resource domain is concerned with the definition, development, and operational aspects of the applications, computing, and networks which represent the infrastructure of an enterprise. |
-| | <schemas>${Resource}<br></schemas> | |
+| | <details>${Resource}<br></details> | |
 | [Engaged Party](https://github.com/tmforum-rand/schemas/tree/master/EngagedParty) | The Engaged Party domain encompasses planning of strategies for Engaged Parties, handling of all types of contact with Engaged Parties, the management of the relationship, and the administration of Engaged Parties data. |
-| | <schemas>${EngagedParty}<br></schemas> | |
+| | <details>${EngagedParty}<br></details> | |
 | [Enterprise](https://github.com/tmforum-rand/schemas/tree/master/Enterprise) | The Enterprise domain provides support and sets policy for the overall business, enterprise or Service Provider. It also includes activities that are common to all enterprises across all industries such as accounting and human resource management. |
-| | <schemas>${Enterprise}<br></schemas> | |
+| | <details>${Enterprise}<br></details> | |
 | [Common](https://github.com/tmforum-rand/schemas/tree/master/Common) | The common domain is of different nature from the above defined ones as the processes, information data and applications described there do not necessarily have relationships. |
-| | <schemas>${Common}<br></schemas> | |
+| | <details>${Common}<br></details> | |
 
 All schemas conform to JSON-Schema draft-07, and to [the design guidelines described here](https://github.com/tmforum-rand/TMF630_REST_API_Design_Guidelines). The schemas are used in the construction of the TM Forum Open-API catalog.
 
